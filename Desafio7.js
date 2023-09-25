e97733094498c309bb21d95531c327277bca8ebe
pedlo07 <pedroalmeidaveber@gmail.com>
2023-09-25 20:36:48 -0300

    update

--- Desafio7.js
+++ Desafio7.js
@@ -1,4 +1,23 @@
-var pedMot = prompt('Digite:',
-'1-Motorista\n',
-'2-Pedestre\n');
+var valor = [1, 4, 5, 7, 12];
+let par = 0;
+let impar = 0;
+let positivo = 0;
+let negativo = 0;
 
+for (i = 0; i < 5; i++){
+    if(valor[i] % 2 == 0){
+        par++;
+        console.log(valor [i] + "é um numero par.");
+    } else if (valor[i] % 2 != 0){
+        impar++;
+        console.log(valor [i] + "é um numero impar.");
+    } else if (valor[i] > 0){
+        positivo++;
+        console.log(valor [i] + "é um numero positivo.");
+    } else if (valor[i] < 0){
+        negativo++;
+        console.log(valor [i] + "é um numero negativo.");
+    }
+
+    console.log("Par: ", par, "Impar: ", impar, "Positivo: ", positivo, "Negativo: ", negativo);
+}
